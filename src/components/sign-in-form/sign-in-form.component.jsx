ae3abde7db87b4aadc8b0fdaad964d5fcf35f57d
Kamal Mango <kamal.mango7@gmail.com>
2022-04-21 13:59:04 +0200

    add cart context and cart dropdown

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -3,7 +3,6 @@ import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
 import "./sign-in-form.styles.scss";
